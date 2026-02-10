c7143c9abd96925587e7f42fb94930534966a213
derekcha-irvinesoft <derekcha@irvinesoft.com>
2026-02-10 09:43:40 -0800

    added dist

---
+++ landing/.vitepress/dist/assets/about.md.DphtZW3V.js
@@ -0,0 +1,1 @@
+import{_ as t,o as e,c as a}from"./chunks/framework.BJybqDTa.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"about.md","filePath":"about.md","lastUpdated":1770663175000}'),o={name:"about.md"};function r(s,c,n,d,p,_){return e(),a("div")}const f=t(o,[["render",r]]);export{m as __pageData,f as default};
